aa679164d5747ff4e95d4a1d767d705fc4483763
Prasad <patilprasad2401@gmail.com>
2023-12-22 11:05:18 +0530

    add practice codes

---
+++ src/main/java/com/prasad/practice/PalindroameNumber.java
@@ -0,0 +1,25 @@
+package com.prasad.practice;
+
+import java.util.Scanner;
+
+public class PalindroameNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Negative number :");
+        int num = sc.nextInt();
+        int result=0,reminder,temp;
+        temp=num;
+        while(num!=0){
+            reminder=num %10;
+            result=result *10 +reminder;
+            num/=10;
+        }
+        if (temp == result)
+            System.out.println(temp +" is palindrome");
+        else
+            System.out.println(temp +" is not palindrome");
+
+
+
+    }
+}
