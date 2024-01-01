f4935741170d9b479e073740038a1e9dd317f537
Prasad <patilprasad2401@gmail.com>
2024-01-01 19:14:25 +0530

    add practice code

--- src/main/java/com/prasad/practice/AscciValue.java
+++
@@ -1,9 +0,0 @@
-package com.prasad.practice;
-
-public class AscciValue {
-    public static void main(String args[]){
-        for (int i=1;i<=98;i++)
-            System.out.println((char)i);
-    }
-
-}
